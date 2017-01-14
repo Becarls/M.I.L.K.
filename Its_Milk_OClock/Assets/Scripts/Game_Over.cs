9b1f69259e7fab21d3d7b3bfd03149989c65cde9
Becarls <becarls@umich.edu>
2017-01-14 17:16:34 -0500

    Was working more on movement and failed at game over

---
+++ Its_Milk_OClock/Assets/Scripts/Game_Over.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Game_Over : MonoBehaviour {
+
+    public GameObject go_text;
+    public GameObject gameover;
+    private CanvasRenderer rend;
+
+    void Start() {
+        
+        rend = gameover.GetComponent<CanvasRenderer>();
+        rend.SetAlpha(0f);
+    }
+
+	void OnTriggerEnter(Collider other) {
+        if (other.name == "Player") {
+            gameover.SetActive(true);
+            go_text.SetActive(true);
+            for (float i = .01f; i < 1; i += .01f) {
+                rend.SetAlpha(i);
+            }
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+}
